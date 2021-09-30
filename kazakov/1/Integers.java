ff042d48680405159cd85690875d90a21df06356
Marat Kazakov <maratka75@inbox.ru>
2021-09-30 22:07:42 +0300

    initial commit

---
+++ kazakov/1/Integers.java
@@ -0,0 +1,18 @@
+
+public class Integers {
+
+	public static void main(String[] args) {
+		Integer val1 = 1;
+		Integer val2 = -1000;
+
+		System.out.println("Hello World!");
+		if( val1 > val2 ) {
+			System.out.printf("Число %s больше чем %s\n", val1, val2);
+		} else if( val1 < val2 ) {
+			System.out.printf("Число %s меньше чем %s\n", val1, val2);
+		} else {
+			System.out.printf("Числа %s и %s равны\n", val1, val2);
+		}
+		System.out.printf("Сумма двух чисел: %s", val1 + val2);
+	}
+}
