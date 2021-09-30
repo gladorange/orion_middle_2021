717b4314542962d072281e97a068dccfdf0774e6
pavlovka <stricker-fer@yandex.ru>
2021-09-30 11:34:59 +0300

    Поправил вывод суммы, добавил перехват ошибки ввода

--- pavlov/1/Main.java
+++ pavlov/1/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,9 +10,7 @@ public class Main {
         return scanner.nextInt();
     }
 
-    public static void main(String[] args) {
-        int firstInteger = readInteger("Введите первое число:");
-        int secondInteger = readInteger("Введите второе число:");
+    public static void calculateAndPrintResult(int firstInteger, int secondInteger){
 
         if(firstInteger > secondInteger){
             System.out.printf("Число %s больше %s \n", firstInteger, secondInteger);
@@ -19,8 +18,19 @@ public class Main {
             System.out.printf("Число %s меньше %s \n", firstInteger, secondInteger);
         }
 
-        long sum = firstInteger + secondInteger;
+        long sum = (long) firstInteger + secondInteger;
 
         System.out.printf("%s + %s = %s", firstInteger, secondInteger, sum);
+    }
+
+    public static void main(String[] args) {
+        try {
+            int firstInteger = readInteger("Введите первое число:");
+            int secondInteger = readInteger("Введите второе число:");
+
+            calculateAndPrintResult(firstInteger, secondInteger);
+        } catch (InputMismatchException e){
+            System.out.println("Введенное значение не является числом типа Integer");
+        }
     }
 }
