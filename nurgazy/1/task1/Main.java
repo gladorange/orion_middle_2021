2c924becb3527f85762f618c284f1c69aac050e4
kenzhekozy <kenzhekozy@gmail.com>
2021-10-01 19:14:26 +0600

    homework 1

---
+++ nurgazy/1/task1/Main.java
@@ -0,0 +1,14 @@
+package task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(compareNumbers(5, 6));
+        System.out.println(compareNumbers(7, 6));
+    }
+
+
+    public static String compareNumbers(Integer a, Integer b) {
+        return (a > b) ? "Число " + a+ " больше " + b : "Число " + a + " меньше " + b;
+    }
+}
