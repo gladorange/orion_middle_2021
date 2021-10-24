c52cbdf4f1ba4e03398cbb74af0c73cd50757cdb
kenzhekozy <kenzhekozy@gmail.com>
2021-10-24 18:56:08 +0600

    homework 3

--- nurgazy/3/task1/FixPriceShop.java
+++ nurgazy/3/task1/FixPriceShop.java
@@ -1,7 +1,6 @@
 package task1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -10,6 +9,21 @@ public class FixPriceShop {
     private int luckyHour;
     final static private int price = 100;
 
+    public FixPriceShop(String[] items) {
+        this.items = items;
+    }
+
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+
+    public int getLuckyHour() {
+        return luckyHour;
+    }
+
+    public void setLuckyHour(int luckyHour) {
+        this.luckyHour = luckyHour;
+    }
 
     int checkItemPrice(String item, int hour) {
         int currentPrice = ((hour == luckyHour) ? price/2 : price);
@@ -43,35 +57,26 @@ public class FixPriceShop {
         System.out.println("товар " + item + " не обнаружен");
     }
 
-
-    public FixPriceShop(String[] items) {
-        this.items = items;
-    }
-
     public static void main(String[] args) {
         FixPriceShop shop1 = new FixPriceShop(new String[]{"Apple", "Orange", "Banana", "Lemon", "Peach", "Mango"});
-        shop1.luckyHour = 12;
+        shop1.setLuckyHour(12);
 
         FixPriceShop shop2 = new FixPriceShop(new String[]{"Phone", "TV", "Radio", "Fridge", "Kettle"});
-        shop2.luckyHour = 10;
+        shop2.setLuckyHour(10);
 
         FixPriceShop shop3 = new FixPriceShop(new String[]{"Snickers", "Mars", "Bounty", "Twicks", "Nesquik"});
-        shop3.luckyHour = 18;
+        shop3.setLuckyHour(15);
 
         String[] itemsFromShop1 = shop1.items;
-
         Random random = new Random();
-
         String currentItem1 = itemsFromShop1[random.nextInt(itemsFromShop1.length)];
-
         for (int i = 0; i < 24; i++) {
-            if (shop1.checkItemPrice(currentItem1, i) == shop1.checkItemPrice(currentItem1, shop1.luckyHour)) {
+            if (shop1.checkItemPrice(currentItem1, i) == shop1.checkItemPrice(currentItem1, shop1.getLuckyHour())) {
                 System.out.println("Оптимальное время для покупки: " + i);
                 shop1.buyItem(currentItem1, i);
                 break;
             }
         }
-
         shop1.buyItem(currentItem1, 1);
     }
 }
